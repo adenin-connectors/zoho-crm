bd6336b087246e9794b76b32bcc3b46e1424fe41
olliejm <olliejm@protonmail.com>
2019-04-11 15:54:51 +0100

    prepare for azure pipeline

--- activities/common/api.js
+++ activities/common/api.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const got = require('got');
 const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
@@ -23,17 +24,13 @@ function api(path, opts) {
     'user-agent': 'adenin Now Assistant Connector, https://www.adenin.com/now-assistant'
   }, opts.headers);
 
-  if (opts.token) {
-    opts.headers.Authorization = `Zoho-oauthtoken ${opts.token}`;
-  }
+  if (opts.token) opts.headers.Authorization = `Zoho-oauthtoken ${opts.token}`;
 
   const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
 
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
+  if (opts.stream) return got.stream(url, opts);
 
-  return got(url, opts).catch(err => {
+  return got(url, opts).catch((err) => {
     throw err;
   });
 }
@@ -47,28 +44,27 @@ const helpers = [
   'delete'
 ];
 
-api.stream = (url, opts) => apigot(url, Object.assign({}, opts, {
+api.stream = (url, opts) => got(url, Object.assign({}, opts, {
   json: false,
   stream: true
 }));
 
 for (const x of helpers) {
   const method = x.toUpperCase();
-  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
-  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
+
 //**filters leads based on provided dateRange */
 api.filterLeadsByDateRange = function (leads, dateRange) {
-  let filteredLeads = [];
-  let timeMin = new Date(dateRange.startDate).valueOf();
-  let timeMax = new Date(dateRange.endDate).valueOf();
+  const filteredLeads = [];
+  const timeMin = new Date(dateRange.startDate).valueOf();
+  const timeMax = new Date(dateRange.endDate).valueOf();
 
   for (let i = 0; i < leads.length; i++) {
-    let createdAtMilis = new Date(leads[i].Created_Time).valueOf();
+    const createdAtMilis = new Date(leads[i].Created_Time).valueOf();
 
-    if (createdAtMilis > timeMin && createdAtMilis < timeMax) {
-      filteredLeads.push(leads[i]);
-    }
+    if (createdAtMilis > timeMin && createdAtMilis < timeMax) filteredLeads.push(leads[i]);
   }
 
   return filteredLeads;
@@ -76,18 +72,27 @@ api.filterLeadsByDateRange = function (leads, dateRange) {
 
 //**maps response data to items */
 api.convertResponse = function (response) {
-  let items = [];
+  const items = [];
   let data = [];
-  if (response.body.data) {
-    data = response.body.data;
-  }
+
+  if (response.body.data) data = response.body.data;
 
   for (let i = 0; i < data.length; i++) {
-    let raw = data[i];
-    let item = { id: raw.id, title: raw.Designation, description: raw.Description, link: `https://crm.zoho.com/crm/`, raw: raw }
+    const raw = data[i];
+    const item = {
+      id: raw.id,
+      title: raw.Designation,
+      description: raw.Description,
+      link: 'https://crm.zoho.com/crm/',
+      raw: raw
+    };
+
     items.push(item);
   }
 
-  return { items: items };
-}
+  return {
+    items: items
+  };
+};
+
 module.exports = api;
